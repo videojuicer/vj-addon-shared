b61f5b6a7b355cc1cc145ff66d9119f2214b4b1d
sixones <adam@4kn.co.uk>
2013-02-28 16:30:51 +0000

    Added underscore + configurator as shared scripts

---
+++ configurator.js
@@ -0,0 +1,115 @@
+var _ = require("underscore");
+
+var configurator = {
+  data: { },
+
+  defaultData: null,
+  presentationData: null,
+
+  url: null,
+  
+  mergeWithCallback: null,
+
+  get: function(key, defaultValue) {
+    var data = this.getConfiguration();
+
+    if (defaultValue == undefined) {
+      defaultValue = null;
+    }
+
+    var value = null;
+
+    if (key.indexOf('.') > -1) {
+      var keys = key.split('.');
+      var parent = data;
+
+      for (var i = 0; i < keys.length; ++i) {
+        if (parent == undefined) {
+          break;
+        }
+
+        parent = parent[keys[i]];
+      }
+
+      value = parent;
+    } else {
+      value = data[key];
+    }
+
+    return (value || player.get(key) || defaultValue);
+  },
+
+  getConfiguration: function() {
+    if (this.defaultData == null && this.presentationData == null) {
+      this.update();
+    }
+
+    return this.data;
+  },
+
+  setMergeWith: function(callback) {
+    this.mergeWithCallback = callback;
+
+    return this;
+  },
+
+  setUrl: function(url) {
+    this.url = url;
+
+    return this;
+  },
+
+  update: function() {
+    this.defaultData = this.fetchDefaultData();
+    this.presentationData = this.fetchPresentationData();
+
+    if (this.callback) {
+      this.data = this.callback(this.defaultData, this.presentationData);
+    } else {
+      this.data = _.extend(this.defaultData, this.presentationData);
+    }
+
+    return this;
+  },
+
+  getJSONUrlFor: function(name) {
+    //return 'http://addon-configs.s3.videojuicer.com/' + player.presentation.seedName + '/o/' + name + '.json';
+    var url = this.url;
+
+    if (url == undefined || url == null) {
+      if (player.presentation.env == 'staging') {
+        url = 'http://addon-configs.s3.amazonaws.com/';
+      } else {
+        url = 'http://addon-configs-staging.s3.amazonaws.com/';
+      }
+
+      url += player.presentation.seedName + '/' + 'uuid' + '/';
+    }
+
+    return 'http://addon-configs.s3.amazonaws.com/myvideojuicer/ecommerce/' + name + '.json';
+  },
+
+  fetchDefaultData: function() {
+    var response = net.http.getSync(this.getJSONUrlFor('defaults'), { });
+    var data = { };
+
+    if (response != null && response.status == 200) {
+      data = JSON.parse(response.body);
+    }
+
+    return data;
+  },
+
+  fetchPresentationData: function() {
+    var response = net.http.getSync(this.getJSONUrlFor(player.presentation.id), { });
+    var data = { };
+
+    if (response != null && response.status == 200) {
+      data = JSON.parse(response.body);
+    }
+
+    return data;
+  }
+};
+
+module.exports = configurator;
